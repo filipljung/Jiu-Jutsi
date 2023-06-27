ddd79ad853e97c4bba12b49ef015c4b24224a6a3
Filipljung <fille-1998@hotmail.com>
2023-06-27 18:33:14 +0500

    Documented the Main.js using detailed comments in code

--- assets/Javascript assests/Main.js
+++ assets/Javascript assests/Main.js
@@ -1,9 +1,13 @@
+/*This array quizData holds the quiz questions, answer options, and correct answers. 
+Each question object contains a question property for the question text, a, b, c, 
+and d properties for the answer options, and a correct property for the correct answer (identified by its corresponding letter).
+*/
 const quizData = [
   {
-    question: "Who is Absolute Champion in BJJ?",
+    question: "Who is the Absolute Champion in BJJ?",
     a: "Nicholas Meregali",
     b: "Gordon Ryan",
-    c: "Bernando Feria",
+    c: "Bernardo Feria",
     d: "Kaynan Duarte",
     correct: "a",
   },
@@ -16,7 +20,7 @@ const quizData = [
     correct: "d",
   },
   {
-    question: "Who won ADCC 2022 in heavyweight?",
+    question: "Who won ADCC 2022 in the heavyweight division?",
     a: "Nicholas Meregali",
     b: "Gordon Ryan",
     c: "Kaynan Duarte",
@@ -24,7 +28,7 @@ const quizData = [
     correct: "b",
   },
   {
-    question: "Who won superfight in ADCC?",
+    question: "Who won the superfight in ADCC?",
     a: "Gordon Ryan",
     b: "Andre Galvao",
     c: "Craig Jones",
@@ -49,6 +53,12 @@ const quizData = [
   },
 ];
 
+
+/*These lines of code retrieve references to various HTML elements using their respective IDs. 
+The quiz variable represents the container element for the quiz, 
+answerEls is a collection of all elements with the class answer (presumably radio buttons for answer options), 
+and questionEl, a_text, b_text, c_text, and d_text represent the elements that display the 
+question and answer options. Finally, submitBtn represents the submit button element. */
 const quiz = document.getElementById('quiz');
 const answerEls = document.querySelectorAll('.answer');
 const questionEl = document.getElementById('question');
@@ -57,45 +67,81 @@ const b_text = document.getElementById('b_text');
 const c_text = document.getElementById('c_text');
 const d_text = document.getElementById('d_text');
 const submitBtn = document.getElementById('submit');
+
+/*These variables are used to keep track of the current question and the user's score. 
+currentQuiz is initially set to 0, indicating that the first question should be displayed. 
+score is initially set to 0, indicating that the user hasn't answered any questions correctly yet. */
 let currentQuiz = 0;
 let score = 0;
 
+//This line calls the loadQuiz() function to initialize the quiz by displaying the first question.
 loadQuiz();
 
-function loadQuiz() {
-  deselectAnswers();
-  const currentQuizData = quizData[currentQuiz];
-  questionEl.innerText = currentQuizData.question;
-  a_text.innerText = currentQuizData.a;
-  b_text.innerText = currentQuizData.b;
-  c_text.innerText = currentQuizData.c;
-  d_text.innerText = currentQuizData.d;
+// Function to load the quiz
+function loadQuiz() 
+{
+
+  deselectAnswers(); // Clear any selected answers
+
+  const currentQuizData = quizData[currentQuiz]; // Get the current quiz data
+
+  questionEl.innerText = currentQuizData.question; // Display the question
+  a_text.innerText = currentQuizData.a; // Display option A
+  b_text.innerText = currentQuizData.b; // Display option B
+  c_text.innerText = currentQuizData.c; // Display option C
+  d_text.innerText = currentQuizData.d; // Display option D
 }
 
-function deselectAnswers() {
-  answerEls.forEach(answerEl => answerEl.checked = false);
+// Function to deselect all answers
+function deselectAnswers() 
+{
+  answerEls.forEach(answerEl => (answerEl.checked = false)); // Uncheck all answer options
 }
 
-function getSelected() {
+// Function to get the selected answer
+function getSelected() 
+{
+
   let answer;
+
   answerEls.forEach(answerEl => {
-    if (answerEl.checked) {
+    if (answerEl.checked)
+    {
+
       answer = answerEl.id;
+
     }
   });
+
   return answer;
 }
 
+// Event listener for submit button
 submitBtn.addEventListener('click', () => {
-  const answer = getSelected();
-  if (answer) {
-    if (answer === quizData[currentQuiz].correct) {
-      score++;
+
+  const answer = getSelected(); // Get the selected answer
+
+  if (answer)
+  {
+
+    if (answer === quizData[currentQuiz].correct)
+    {
+      score++; // Increase the score if the selected answer is correct
+
     }
-    currentQuiz++;
-    if (currentQuiz < quizData.length) {
-      loadQuiz();
-    } else {
+
+    currentQuiz++; // Move to the next question
+
+    if (currentQuiz < quizData.length) 
+    {
+
+      loadQuiz(); // Load the next question
+
+    } 
+    
+    else 
+    {
+      // Display the final score and reload button
       quiz.innerHTML = `
         <h2>You answered ${score}/${quizData.length} questions correctly</h2>
         <button onclick="location.reload()">Reload</button>
